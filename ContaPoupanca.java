11f4f46cb4916e74b3ab55bcec329903ba679753
isabelasnts <101901724+isabelasnts@users.noreply.github.com>
2022-07-08 20:18:58 -0300

    Update and rename contaPoupanca.java to ContaPoupanca.java

--- ContaPoupanca.java
+++ ContaPoupanca.java
@@ -1,6 +1,6 @@
-public class contaPoupanca extends Conta{
+public class ContaPoupanca extends Conta{
 
-    public contaPoupanca(Cliente cliente){
+    public ContaPoupanca(Cliente cliente){
         super (cliente);
     }
 
